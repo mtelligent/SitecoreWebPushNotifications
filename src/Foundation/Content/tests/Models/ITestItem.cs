c10c47971666f554381eb89adfdbff454f319d2f
David San Filippo <david.sanfilippo@perficient.com>
2020-08-20 15:18:04 -0400

    Removed Unneeded Helixbase Projects

--- src/Foundation/Content/tests/Models/ITestItem.cs
+++
@@ -1,9 +0,0 @@
-using Toast.Foundation.ORM.Models;
-
-namespace Toast.Foundation.Content.Tests.Models
-{
-    public interface ITestItem : IGlassBase
-    {
-        string Title { get; set; }
-    }
-}
